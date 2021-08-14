c1d839b6860177de58ad5c9b6afc4775cd467c15
mugglewei <mugglewei@gmail.com>
2021-08-14 11:56:42 +0800

    fix compile error in windows

--- muggle/cpp/net/socket_handle.h
+++ muggle/cpp/net/socket_handle.h
@@ -35,11 +35,13 @@ public:
 	/**
 	 * @brief socket callback function - on socket success bind addr
 	 */
+	MUGGLE_CPP_EXPORT
 	virtual void onBind(muggle_socket_event_t *ev, SocketPeer *peer);
 
 	/**
 	 * @brief socket callback function - on socket success listen addr
 	 */
+	MUGGLE_CPP_EXPORT
 	virtual void onListen(muggle_socket_event_t *ev, SocketPeer *peer);
 
 	/**
